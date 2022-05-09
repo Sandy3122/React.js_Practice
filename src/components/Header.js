1e0c06292fa38a2eede11641d57a8ec8a72d0632
Sandeep <seeram87@gmail.com>
2022-05-09 13:17:55 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './style.css'
+function Header(){
+
+    return(
+        
+        <>
+        <h1>Indian Premier League</h1>
+        </>
+    )
+
+}
+ 
+export default Header;
---
+++ src/components/Teams.js
@@ -0,0 +1,51 @@
+import './style.css'
+function Teams(){
+
+    return(
+        <div className="teams-item">
+            {/* <div>Team</div>
+            <div>Bats Man</div>
+            <div>Bowler</div>
+            <div>All-Rounder</div>
+            <br/> */}
+
+
+            <table id="customers">
+  <tr>
+    <th>Team</th>
+    <th>Bats Man</th>
+    <th>Bowler</th>
+    <th>All-Rounder</th>
+  </tr>
+  <tr>
+    <td>CSK</td>
+    <td>7</td>
+    <td>4</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>SRH</td>    
+    <td>5</td>    
+    <td>3</td>    
+    <td>3</td>    
+  </tr>
+  <tr>
+    <td>SRH</td>    
+    <td>5</td>    
+    <td>3</td>    
+    <td>3</td>    
+  </tr>
+  <tr>
+    <td>SRH</td>    
+    <td>5</td>    
+    <td>3</td>    
+    <td>3</td>    
+  </tr>
+  
+</table>
+
+        </div>
+    )
+}
+
+export default Teams;
